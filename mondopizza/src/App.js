5dcb5eb9c85490e41be8f845656d9956717bd9a1
Mattia-Zanini <christian.mondini@iisviolamarchesini.edu.it>
2022-05-13 09:17:39 +0200

    3
    3

--- mondopizza/src/App.js
+++ mondopizza/src/App.js
@@ -1,59 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Card from './components/card';
 import margherita from './images/margherita.jpg';
 import diavola from './images/diavola.jpg';
 import vegetariana from './images/vegetariana.jpg';
-import Index from './index.css';
+//import { BrowserRouter as Router, Switch, Route } from 'react-router';
+
+
 
 class App extends Component {
-  state= {
-    cards:[
-      {id: 0,nome: "margherita",prezzo: 5.99,immagine: margherita,quantità: 0},
-      {id: 1,nome: "vegetariana",prezzo: 8.99,immagine: vegetariana,quantità: 0},
-      {id: 2,nome: "diavola",prezzo: 7.99,immagine: diavola,quantità: 0},
-      
-    ]
-  }
+    
+    state = {
+        cards: [
+            { id: 0, nome: "margherita", prezzo: 5.99, immagine: margherita, quantità: 0 },
+            { id: 1, nome: "vegetariana", prezzo: 8.99, immagine: vegetariana, quantità: 0 },
+            { id: 2, nome: "diavola", prezzo: 7.99, immagine: diavola, quantità: 0 },
 
-  handleIncrement = card=>{
-    const cards=[...this.state.cards];
-    const id=cards.indexOf(card);
-    cards[id]={...card};
-    cards[id].quantità++;
-    this.setState({cards});
-  }
+        ]
+    }
 
-  handleDecrement = card=>{
-    const cards=[...this.state.cards];
-    const id=cards.indexOf(card);
-    cards[id]={...card};
-    cards[id].quantità--;
-    this.setState({cards});
-  }
+    handleIncrement = card => {
+        const cards = [...this.state.cards];
+        const id = cards.indexOf(card);
+        cards[id] = { ...card };
+        cards[id].quantità++;
+        this.setState({ cards });
+    }
 
-  render() {
-    return (
-      <>
-        < Navbar />
-        <div className='container'>
-          <h1>Cosa vuoi ordinare?</h1>
-          <hr />
-          <div className='row'>
-            {this.state.cards.map(card=>(
-              <Card
-              key={card.id}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
-              card={card} />
-            ))}
-          </div>
-        </div>
+    handleDecrement = card => {
+        const cards = [...this.state.cards];
+        const id = cards.indexOf(card);
+        cards[id] = { ...card };
+        cards[id].quantità--;
+        this.setState({ cards });
+    }
 
-      </>
+    render() {
+        
+        return (
+            //<Router>
+            <>
+                <Navbar />
+                
+                <div className='container' >
+                    <h1> Cosa vuoi ordinare ?</h1>
+                    <hr />
+                    <div className='row' > {
+                        this.state.cards.map(card => (<
+                            Card key={card.id}
+                            onIncrement={this.handleIncrement}
+                            onDecrement={this.handleDecrement}
+                            card={card}
+                        />))
+                    } </div>
+                </div>
 
-    );
-  }
+            </>
+            //</Router>
+        );
+    }
+    
 }
-
 export default App;
--- mondopizza/src/components/card.js
+++ mondopizza/src/components/card.js
@@ -5,7 +5,7 @@ class Card extends Component{
     render() {
         return (
             <div className='col'>
-                <div className="card" style={{ width: '15rem', textAlign: 'center' }}>
+                <div className="card" style={{ width: '15rem', textAlign: 'center', backgroundColor: 'white' }}>
                     <img src={this.props.card.immagine} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{this.props.card.nome}</h5>
